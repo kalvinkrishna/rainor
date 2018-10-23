c998a0fb879e44768beacea9144c521e613bb5e3
Calvin <calvin.krishna@technopartner.id>
2018-10-23 09:48:29 +0700

    add refresh product cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,24 +3,32 @@ var click = true;
 $(document).ready(() => {
     getList();
 
-    $(".deleteProduct, .delete-count").on('click',function(){
+    $(".deleteProduct").on('click',function(){
         
-        if($(this).hasClass('delete-count')){
-            deleteProduct($(this).data('items'),$(this).parents().find('.quantity').val());
-            getList();
-            if($(this).parents().find('.quantity').val() == 0){
-                $(this).parents(".media").remove();
-            }
-        } else {
-            deleteProduct($(this).data('items'));   
-            getList();
-            $(this).parents(".media").remove();
-        }
+        deleteProduct($(this).data('items'));   
+        getList();
+        $(this).parents(".media").remove();
+        
     });
 
-    $(".plus-count").on('click',function(){
+    $(document).on('click','.delete-count',function(){
+        deleteProduct($(this).data('items'),$(this).parents().find('.quantity').val());
+        getList();
+        if($(this).parents().find('.quantity').val() == 0){
+                $(this).parents(".media").remove();
+        }
+    });
+    $(document).on('click','.plus-count',function(){
         addCart($(this).data('items'));
         getList();
+    });
+
+    $(document).on('click','.countdown',function(){
+        this.parentNode.querySelector("input[type='number']").stepDown();
+    });
+
+    $(document).on('click','.countup',function(){
+        this.parentNode.querySelector("input[type='number']").stepUp();
     });
 });
 
@@ -67,12 +75,74 @@ getList = () => {
         url: baseurl + "/cart/list/all",
         method: 'GET',
         success:function(result_dua){
-            
+           
             if(result_dua.status == "success"){
+                let data = result_dua.result.data;
+            
+                $('.cartcontent').html("");
+                $.each(data,function(index,value){
+                    var imagePath = baseurl+'/image/';
+                    try{
+                        imagePath += value.photo[0].url;
+
+                        let template = '<div class="media">';
+                            template += '<span aria-hidden="true" class="deleteProduct" data-items="'+value.id+'">×</span>';
+                            template += ' <img class="mr-3 img-thumbnail col-5" src="'+imagePath+'">';
+                            template += '<div class="media-body">';
+                            template += '<h5 class="mt-0">'+value.product_name+'</h5>';
+
+                            template += '<div class="mt-12">';
+                            template += ' <span>Hitam</span> - <span>XXL</span>';
+                            template += '</div>';
+
+                            template += '<br>';
+                            template += ' <div class="mt-12">Rp.'+value.price+'</div>';
+                            template += ' <div class="number-input">';
+                            template += ' <button class="delete-count countdown" data-items="'+value.id+'"></button>';
+                            template += ' <input class="quantity" min="0" name="quantity" value="'+value.count+'" type="number">';
+                            template += ' <button class="plus plus-count countup" data-items="'+value.id+'"></button>';
+                            template += '</div>';
+                            template += '</div>';
+                        template += '</div>';
+
+                        $('.cartcontent').append(template);
+
+                    } catch(e){
+
+
+                        let template = '<div class="media">';
+                            template += '<span aria-hidden="true" class="deleteProduct" data-items="'+value.id+'">×</span>';
+                            template += ' <img class="mr-3 img-thumbnail col-5" src="'+imagePath+'500x500.png">';
+                            template += '<div class="media-body">';
+                            template += '<h5 class="mt-0">'+value.product_name+'</h5>';
+
+                            template += '<div class="mt-12">';
+                            template += ' <span>Hitam</span> - <span>XXL</span>';
+                            template += '</div>';
+
+                            template += '<br>';
+                            template += ' <div class="mt-12">Rp.'+value.price+'</div>';
+                            template += ' <div class="number-input">';
+                            template += ' <button class="delete-count countdown" data-items="'+value.id+'"></button>';
+                            template += ' <input class="quantity" min="0" name="quantity" value="'+value.count+'" type="number">';
+                            template += ' <button class="plus plus-count countup" data-items="'+value.id+'"></button>';
+                            template += '</div>';
+                            template += '</div>';
+                        template += '</div>';
+
+
+                        $('.cartcontent').append(template);
+
+                        return true;
+                    }
+                });
+               
+                
                 $(".count").html(result_dua.result.count);
                 $(".total").html(result_dua.result.price);
                 $(".totalbarang").html(result_dua.result.count);
             }
+
         }
     });
 }
