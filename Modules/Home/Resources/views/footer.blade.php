abe47b4e6af767c2483a5bf65c26b11156c13bd2
KalvinKRishna <calvin.krishna@technopartner.id>
2018-10-30 01:02:08 +0700

    perubahan di footer

--- Modules/Home/Resources/views/footer.blade.php
+++ Modules/Home/Resources/views/footer.blade.php
@@ -1,22 +1,16 @@
 @section('footer')
     @include('home::cart.cart')
     <section class='footer container-fluid'>
+        <div class="container">
         <div class='row'>
-            <div class='col-4'>
-                <ul class="nav flex-column">
-                    <li class='nav-link'> <a class="nav-link active" href="#">promo</a></li>
-                    <li class='nav-link'><a class="nav-link" href="#">ootd</a></li>
-                    <li class='nav-link'><a class="nav-link" href="#">geng irit</a></li>
-                </ul>
- 
-            </div>
-            <div class='col-4'>
+            
+            <div class='col-6 text-center'>
                 <ul class="nav flex-column">
                     <li class='nav-link'> <a class="nav-link active" href="#">tentang salestok</a></li>
                     <li class='nav-link'><a class="nav-link" href="#">karir</a></li>
                 </ul>
             </div>
-            <div class="col-4">
+            <div class="col-6 text-center">
             
                 <ul class="nav flex-column">
                     <li class='nav-link'> <a class="nav-link active" href="#">tentang sista</a></li>
@@ -24,6 +18,7 @@
                     <li class='nav-link'><a class="nav-link" href="#">poin</a></li>
                 </ul>
             </div>
+        </div>
         </div>
     </section>
 @endsection
